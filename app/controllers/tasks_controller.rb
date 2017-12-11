d684acd57d75127984d9031eedb3437ca6e8c7d2
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-11 10:02:45 +0100

    problem z routers

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+  def index
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destory
+  end
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'task/index'
-
+  # root 'task#index'
+  resources :task
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
