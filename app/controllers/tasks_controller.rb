efa42bbafbd34173bda393d1218de58f5fbd9773
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-11 13:57:45 +0100

    dodawanie, wyświetlanie

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all.order("")
   end
   def new
     @task = Task.new
   end
   def create
+    @task = Task.create(title: ' ', content: ' ')
   end
   def edit
   end
