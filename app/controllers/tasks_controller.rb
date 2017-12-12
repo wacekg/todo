4f278385100a750c21258632ef5cd8b15a360342
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-12 10:21:48 +0100

    Create ok, pozostałe jeszcze nie

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,22 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all.order("")
+    @tasks = Task.all.order(created_at: :desc)
   end
   def new
     @task = Task.new
   end
   def create
-    @task = Task.create(title: ' ', content: ' ')
+    @task = Task.create(params[:task].permit(:title, :content))
   end
   def edit
   end
   def update
   end
-  def destory
+  def destroy
+    @task.destroy
+    redirect_to task_path
   end
   def show
+    @task = Task.where() #warunek wybranego taska lub .find(Id wybranego taska)
   end
 end
